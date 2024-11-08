b2a2188d4e288d596ed969ef7932d0940e358e3f
Jyoti052 <169053741+Jyoti052@users.noreply.github.com>
2024-11-08 08:25:37 +0000

    dates done

---
+++ 01_basics/datesinJS.js
@@ -0,0 +1,18 @@
+let myDate = new Date()
+//console.log(myDate.toString);
+
+let myCreatedDate = new Date('01-14-2023')
+//console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+//console.log(myTimeStamp);
+
+//console.log(myCreatedDate.getTime());
+
+//console.log(Math.floor(Date.now()/1000));//milliseconds to seconds
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getDay());
+console.log(newDate.getMonth());
+console.log(newDate.getMinutes());
