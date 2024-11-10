e654eb4e418d6b89d3bededc0e2c3631e3882813
Jyoti052 <169053741+Jyoti052@users.noreply.github.com>
2024-11-10 08:11:34 +0000

    objects_02

--- 01_basics/objects_02.js
+++ 01_basics/objects_02.js
@@ -4,5 +4,30 @@
 
 const tinderUser = {}
 tinderUser.id = '123abc'
+tinderUser.name = 'Sammy'
+tinderUser.isLoggedIn = false
 
-console.log(tinderUser)
+//console.log(tinderUser)
+
+
+const regularUser = {
+    email:"some@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:'jyoti',
+            lastname:'thakur'
+        }
+    }
+}
+
+//console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"a",4:"b"}
+
+//const obj3 = {obj1,obj2} // will give obj1 and obj2 as two objects
+
+const obj3 = Object.assign({},obj1,obj2)
+
+console.log(obj3);
+
