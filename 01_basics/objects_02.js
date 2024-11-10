0ea703d98a39eee3d19991be0395fb0eb293dcf0
Jyoti052 <169053741+Jyoti052@users.noreply.github.com>
2024-11-10 04:47:37 +0000

    objects_02

---
+++ 01_basics/objects_02.js
@@ -0,0 +1,8 @@
+//singleton object made with the help of constructor--->
+//const tinderUser = new Object()
+//console.log(tinderUser)
+
+const tinderUser = {}
+tinderUser.id = '123abc'
+
+console.log(tinderUser)
