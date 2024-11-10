c9cb2712e16bc3ba80c255f7f7622dff371bc3bf
Jyoti052 <169053741+Jyoti052@users.noreply.github.com>
2024-11-10 14:47:20 +0000

    objects_02

--- 01_basics/objects_02.js
+++ 01_basics/objects_02.js
@@ -24,10 +24,39 @@ const regularUser = {
 
 const obj1 = {1:"a",2:"b"}
 const obj2 = {3:"a",4:"b"}
+const obj4 = {5:"a",6:"b"}
 
 //const obj3 = {obj1,obj2} // will give obj1 and obj2 as two objects
 
-const obj3 = Object.assign({},obj1,obj2)
+//const obj3 = Object.assign({},obj1,obj2,obj4)//will combine these objects into a single object
 
-console.log(obj3);
+const obj3 = {...obj1, ...obj2} // another way of combining 
+
+//console.log(obj3);
+
+const users = [
+    {
+        id:1,
+        email:"h@gmail.com"
+    },
+    {
+        id:2,
+        email:"j@gmail.com"
+    },
+    {
+        id:3,
+        email:"y@gmail.com"
+    }
+]
+
+//console.log(users[0].email)
+
+console.log(tinderUser)
+//console.log(Object.keys(tinderUser));
+//console.log(Object.values(tinderUser));
+//console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedOf'));// to check if this property exists or not
+
+
 
